0ab8809d43aad4782dbb9388d558cc8d3cf6dbe6
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-08-16 14:05:42 +0300

    Add json response

--- src/main/java/org/example/vertx_starter/MainVerticle.java
+++ src/main/java/org/example/vertx_starter/MainVerticle.java
@@ -2,22 +2,37 @@ package org.example.vertx_starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.spi.observability.HttpRequest;
 
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        vertx.createHttpServer()
+            .requestHandler(MainVerticle::handle)
+            .listen(
+                8888,
+                http -> {
+                    if (http.succeeded()) {
+                        startPromise.complete();
+                        System.out.println("HTTP server started on port 8888");
+                    } else {
+                        startPromise.fail(http.cause());
+                    }
+                }
+            );
+    }
+
+    private static void handle(HttpServerRequest req) {
+        JsonObject object = new JsonObject()
+            .put("name", "Vert.x Starter")
+            .put("version", "1.0.0")
+            .put("current-time", System.currentTimeMillis())
+            .put("javaVersion", System.getProperty("java.version"));
+        req.response()
+            .putHeader("content-type", "application/json")
+            .end(object.toString());
+    }
 }
